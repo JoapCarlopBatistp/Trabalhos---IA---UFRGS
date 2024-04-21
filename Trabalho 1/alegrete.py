b5d4cafb65013723e50198d5789ad9c089fdb408
JoapCarlop <joocarlos.batista@gmail.com>
2024-04-21 16:17:23 -0300

    Função para calcular Erro Quadrático Médio feita

--- Trabalho 1/alegrete.py
+++ Trabalho 1/alegrete.py
@@ -9,7 +9,13 @@ def compute_mse(b, w, data):
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :return: float - o erro quadratico medio
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    x = data[:,0]                                                   #Primeira coluna do parâmetro "data" é separada (X)
+    y = data[:,1]                                                   #Segunda coluna do parâmetro "data" é separada (Y)
+    N = data.shape[0]                                               #Conta a quantidade de combinações (x, y) na matriz
+    y_prediction = w*x + b                                          #Substitui X na função de regressão linear para encontrar o Y predito pela função
+    mse = np.divide(np.sum((y - y_prediction)**2, axis=0), N)       #Calcula a função de loss (somatório dos quadrados das difetenças de y por y predito dividido pelo número de pares (x,y))
+    
+    return mse                                                      #Retorna o valor float de loss
 
 
 def step_gradient(b, w, data, alpha):
@@ -40,3 +46,4 @@ def fit(data, b, w, alpha, num_iterations):
     :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
     """
     raise NotImplementedError  # substituir pelo seu codigo
+
